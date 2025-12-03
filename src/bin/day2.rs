d1f9d4af3ad6b0547f1d2bbdf136f998a98c4d6d
Dorumin <dorminsama@gmail.com>
2025-12-02 23:32:38 -0300

    rep(ux)r(s)ieve

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,9 +1,27 @@
-use std::{fmt::Write, ops::RangeInclusive};
+use std::{collections::HashSet, fmt::Write, ops::RangeInclusive};
 
 const INPUT: &str = include_str!("../inputs/day2.txt");
 
 struct Elfilter {
     ranges: Vec<RangeInclusive<u64>>
+}
+
+/// I wish I could use doctests in binaries
+fn split_pieces(n: u64, piece_digits: u32) -> impl Iterator<Item = u64> {
+    assert!(n > 0 && piece_digits > 0);
+
+    let number_digits = Elfilter::count_digits(n);
+    assert!(number_digits.is_multiple_of(piece_digits));
+
+    let piece_divisor = 10u64.pow(piece_digits);
+
+    std::iter::successors(Some((number_digits - piece_digits) as i32), move |&d| {
+        let next = d - piece_digits as i32;
+        (next >= 0).then_some(next)
+    })
+    .map(move |digits_to_truncate| {
+        (n / 10u64.pow(digits_to_truncate as u32)) % piece_divisor
+    })
 }
 
 impl Elfilter {
@@ -16,6 +34,24 @@ impl Elfilter {
                 first..=last
             }).collect()
         }
+    }
+
+    fn normalize_ranges_log10(&mut self) {
+        self.ranges = std::mem::take(&mut self.ranges).into_iter().flat_map(|r| {
+            let s = r.start().ilog10();
+            let e = r.end().ilog10();
+
+            (s..=e).map(move |i| {
+                let start = 10u64.pow(i);
+                let end = 10u64.pow(i + 1) - 1;
+
+                start.max(*r.start())..=end.min(*r.end())
+            })
+        }).collect();
+    }
+
+    fn count_digits(num: u64) -> u32 {
+        num.ilog10() + 1
     }
 
     fn simple_sieve(&self) -> u64 {
@@ -41,6 +77,7 @@ impl Elfilter {
         invalidsum
     }
 
+    #[allow(unused)]
     fn repeat_sieve(&self) -> u64 {
         let mut buf = String::new();
         let mut invalidsum = 0;
@@ -74,13 +111,94 @@ impl Elfilter {
                         continue 'numbers_loop;
                     }
                 }
-                // if buf.len().is_multiple_of(2) {
-                //     let mid = buf.len() / 2;
+            }
+        }
 
-                //     if buf[..mid] == buf[mid..] {
-                //         invalidsum += n;
-                //     }
-                // }
+        invalidsum
+    }
+
+    fn pux_sieve(&mut self) -> u64 {
+        self.normalize_ranges_log10();
+
+        let mut witness_me = HashSet::new();
+        let mut invalidsum = 0;
+
+        for range in self.ranges.iter() {
+            let lower_bound = *range.start();
+            let upper_bound = *range.end();
+
+            assert!(1 <= lower_bound && lower_bound <= upper_bound);
+
+            let bound_digits = Self::count_digits(lower_bound);
+
+            assert_eq!(bound_digits, Self::count_digits(upper_bound));
+
+            for piece_digits in 1..(bound_digits / 2 + 1) {
+                if !bound_digits.is_multiple_of(piece_digits) {
+                    continue
+                }
+
+                let num_pieces = bound_digits / piece_digits;
+                let mut lower_bound_pieces = split_pieces(lower_bound, piece_digits);
+                let mut lower_piece_bound = lower_bound_pieces.next().unwrap();
+
+                for next_piece in lower_bound_pieces {
+                    if lower_piece_bound == next_piece {
+                        continue
+                    }
+
+                    if lower_piece_bound < next_piece {
+                        lower_piece_bound += 1
+                    }
+
+                    break
+                }
+
+                let mut upper_bound_pieces = split_pieces(upper_bound, piece_digits);
+                let mut upper_piece_bound = upper_bound_pieces.next().unwrap();
+
+                for next_piece in upper_bound_pieces {
+                    if upper_piece_bound == next_piece {
+                        continue
+                    }
+
+                    if upper_piece_bound > next_piece {
+                        upper_piece_bound -= 1
+                    }
+
+                    break
+                }
+
+                if lower_piece_bound > upper_piece_bound {
+                    continue
+                }
+
+                let piece_divisor = 10u64.pow(piece_digits);
+                let mut delta = 1;
+
+                for _ in 0..(num_pieces - 1) {
+                    delta = (delta * piece_divisor) + 1;
+                }
+
+                let mut invalid_product_id = delta * lower_piece_bound;
+
+                assert!(lower_bound <= invalid_product_id && invalid_product_id <= upper_bound);
+
+                if !witness_me.contains(&invalid_product_id) {
+                    invalidsum += invalid_product_id;
+                    witness_me.insert(invalid_product_id);
+                }
+
+                for _ in (lower_piece_bound + 1)..=upper_piece_bound {
+                    invalid_product_id += delta;
+
+                    assert!(lower_bound <= invalid_product_id && invalid_product_id <= upper_bound);
+                    if !witness_me.contains(&invalid_product_id) {
+                        invalidsum += invalid_product_id;
+                        witness_me.insert(invalid_product_id);
+
+                    }
+                }
             }
         }
 
@@ -95,9 +213,9 @@ fn part1() {
 }
 
 fn part2() {
-    let filter = Elfilter::parse(INPUT);
+    let mut filter = Elfilter::parse(INPUT);
 
-    dbg!(filter.repeat_sieve());
+    dbg!(filter.pux_sieve());
 }
 
 fn main() {
@@ -131,5 +249,19 @@ mod tests {
         let filter = Elfilter::parse(EXAMPLE);
 
         assert_eq!(filter.repeat_sieve(), 4174379265);
+    }
+
+    #[test]
+    fn example_pux() {
+        let mut filter = Elfilter::parse(EXAMPLE);
+
+        assert_eq!(filter.pux_sieve(), 4174379265);
+    }
+
+    #[test]
+    fn splits() {
+        assert_eq!(split_pieces(123456, 1).collect::<Vec<_>>(), vec![1, 2, 3, 4, 5, 6]);
+        assert_eq!(split_pieces(123456, 2).collect::<Vec<_>>(), vec![12, 34, 56]);
+        assert_eq!(split_pieces(123456, 3).collect::<Vec<_>>(), vec![123, 456]);
     }
 }
